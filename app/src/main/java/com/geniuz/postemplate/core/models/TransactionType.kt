83b48a5224b572e6528f3118fab1e7c496ded8ad
geniuzzz <oscarvictory30@gmail.com>
2023-07-01 15:29:20 +0100

    add code to transaction type

--- app/src/main/java/com/geniuz/postemplate/core/models/TransactionType.kt
+++ app/src/main/java/com/geniuz/postemplate/core/models/TransactionType.kt
@@ -1,7 +1,7 @@
 package com.geniuz.postemplate.core.models
 
-enum class TransactionType {
-    PURCHASE,
+enum class TransactionType(val code: Int = 0x00) {
+    PURCHASE(0x00),
     PURCHASE_WITH_CASHBACK,
     REVERSAL,
     REFUND,
