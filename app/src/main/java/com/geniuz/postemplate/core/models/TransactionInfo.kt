7a246c1850f8c21563841b675646bca9f7e182e4
geniuzzz <oscarvictory30@gmail.com>
2023-07-01 15:28:50 +0100

    add more properties to transaction info

--- app/src/main/java/com/geniuz/postemplate/core/models/TransactionInfo.kt
+++ app/src/main/java/com/geniuz/postemplate/core/models/TransactionInfo.kt
@@ -4,6 +4,8 @@ data class TransactionInfo(
     val amount: Double,
     val cashbackAmount: Double = 0.0,
     val transactionType: TransactionType,
+    val rrn: String,
+    val stan: String,
     val terminalInfo: TerminalInfo,
-    val cardInfo: CardInfo
+    val cardInfo: CardInfo? = null
 )
