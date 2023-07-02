dd6a99cdab787ea4ae6744f5de14a3565df56c08
geniuzzz <oscarvictory30@gmail.com>
2023-07-02 11:04:06 +0100

    add online process state to the emv flow

--- app/src/main/java/com/geniuz/postemplate/core/emv/EMVProcess.kt
+++ app/src/main/java/com/geniuz/postemplate/core/emv/EMVProcess.kt
@@ -2,6 +2,7 @@ package com.geniuz.postemplate.core.emv
 
 import com.geniuz.postemplate.core.models.CardApplication
 import com.geniuz.postemplate.core.models.CardInfo
+import com.geniuz.postemplate.core.models.TransactionInfo
 
 sealed class EMVProcess {
     object Init: EMVProcess()
@@ -10,12 +11,11 @@ sealed class EMVProcess {
     object OnBeforeGPO : EMVProcess()
     object OnGPO : EMVProcess()
     data class OnCardHolderInputPinRequired(val isOnlinePin: Boolean, val pinTrialsLeft: Int) : EMVProcess()
-
     data class OnPinEntered(val pinData: String): EMVProcess()
     object OnCtlsTapAgain : EMVProcess()
+    data class OnlineProcessRequired(val transactionInfo: TransactionInfo): EMVProcess()
     object OnRemoveCard : EMVProcess()
     object OnFinish : EMVProcess()
-
     data class OnInfo(val message: String): EMVProcess()
     data class OnError(val error: Throwable): EMVProcess()
 }
